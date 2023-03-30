ef3b305fcf9b8b2a1f1aab84ed92e9e82d7b4f42
avr87 <annavanruiten@gmail.com>
2023-03-30 17:11:50 +0100

    added basic search form and way to collect input value

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       Hello
-      </header>
+      <div className="container">
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="text-center"><a href="https://github.com/avr87/dictionary-app.git">Coded</a> by Anna van Ruiten  and hosted on <a href="https://animated-yeot-898830.netlify.app">Netlify</a> </footer>
+      </div>
     </div>
   );
 }
